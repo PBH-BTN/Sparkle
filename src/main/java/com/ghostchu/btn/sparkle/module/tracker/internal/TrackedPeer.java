07e8ec8fd2eb42508284deda3df90f7e481859b0
Ghost_chu <ghostchu@111.com>
2024-10-09 04:40:58 +0800

    删除 peerPort 的约束

--- src/main/java/com/ghostchu/btn/sparkle/module/tracker/internal/TrackedPeer.java
+++ src/main/java/com/ghostchu/btn/sparkle/module/tracker/internal/TrackedPeer.java
@@ -15,7 +15,7 @@ import java.time.OffsetDateTime;
 
 @Entity
 @Table(name = "tracker_peers",
-        uniqueConstraints = {@UniqueConstraint(columnNames = {"peerIp", "peerId", "peerPort", "torrentInfoHash"})},
+        uniqueConstraints = {@UniqueConstraint(columnNames = {"peerIp", "peerId", "torrentInfoHash"})},
         indexes = {@Index(columnList = "peerId"), @Index(columnList = "peerIp"), @Index(columnList = "torrentInfoHash")}
 )
 @AllArgsConstructor
