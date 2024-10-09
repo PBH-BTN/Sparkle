c655c20eb4ff90cf151a997060398c7bae84500e
Ghost_chu <ghostchu@111.com>
2024-10-09 15:30:39 +0800

    修复主键冲突问题

--- src/main/java/com/ghostchu/btn/sparkle/module/tracker/internal/TrackedPeerRepository.java
+++ src/main/java/com/ghostchu/btn/sparkle/module/tracker/internal/TrackedPeerRepository.java
@@ -32,7 +32,7 @@ public interface TrackedPeerRepository extends SparkleCommonRepository<TrackedPe
     @Query(value = """
             INSERT INTO tracker_peers (req_ip, peer_id, peer_id_human_readable, peer_ip, peer_port, torrent_info_hash, uploaded_offset, downloaded_offset, "left", last_event, user_agent, last_time_seen, peer_geoip, request_geoip, version) \
             VALUES (:reqIp, :peerId, :peerIdHumanReadable, :peerIp, :peerPort, :torrentInfoHash, :uploadedOffset, :downloadedOffset, :left, :lastEvent, :userAgent, :lastTimeSeen, CAST(:peerGeoIP AS jsonb), CAST(:requestGeoIP AS jsonb), :version) \
-            ON CONFLICT (peer_ip, peer_id, torrent_info_hash) DO UPDATE SET \
+            ON CONFLICT (peer_id, torrent_info_hash) DO UPDATE SET \
             uploaded = CASE \
               WHEN tracker_peers.uploaded_offset > EXCLUDED.uploaded_offset THEN tracker_peers.uploaded + EXCLUDED.uploaded_offset \
               ELSE tracker_peers.uploaded + (EXCLUDED.uploaded_offset - tracker_peers.uploaded_offset) \
