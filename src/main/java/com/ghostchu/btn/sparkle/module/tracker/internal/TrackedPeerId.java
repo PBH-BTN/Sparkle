99bdd90c40798d249f36cf7d120f472d94ebf343
Ghost_chu <ghostchu@111.com>
2024-10-09 14:37:44 +0800

    Revert "修复查询错误"
    This reverts commit d99b3b76876b7f3abab83bbcf652366f84f561ad.

--- src/main/java/com/ghostchu/btn/sparkle/module/tracker/internal/TrackedPeerId.java
+++ src/main/java/com/ghostchu/btn/sparkle/module/tracker/internal/TrackedPeerId.java
@@ -1,7 +1,6 @@
 package com.ghostchu.btn.sparkle.module.tracker.internal;
 
 import jakarta.persistence.Column;
-import jakarta.persistence.Embeddable;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -13,7 +12,6 @@ import org.hibernate.annotations.DynamicUpdate;
 @Getter
 @Setter
 @DynamicUpdate
-@Embeddable
 public class TrackedPeerId {
     @Column(nullable = false)
     private String peerId;
