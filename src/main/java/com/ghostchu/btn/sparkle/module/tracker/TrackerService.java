c6e3fba99386d6c94575bf948e12b490a070155d
Ghost_chu <ghostchu@111.com>
2024-10-10 23:43:50 +0800

    修复锁未在所有分支中释放的问题

--- src/main/java/com/ghostchu/btn/sparkle/module/tracker/TrackerService.java
+++ src/main/java/com/ghostchu/btn/sparkle/module/tracker/TrackerService.java
@@ -101,12 +101,12 @@ public class TrackerService {
             log.info("Skipped this round announce flush, another task is running. ");
             return;
         }
-        if (!announceDeque.isEmpty()) {
-            log.info("开始保存 Peers");
-        } else {
-            return;
-        }
         try {
+            if (!announceDeque.isEmpty()) {
+                log.info("开始保存 Peers，异步宣告队列大小：{}", announceDeque.size());
+            } else {
+                return;
+            }
             try (ExecutorService flushService = Executors.newVirtualThreadPerTaskExecutor()) {
                 while (true) {
                     var announce = announceDeque.poll();
