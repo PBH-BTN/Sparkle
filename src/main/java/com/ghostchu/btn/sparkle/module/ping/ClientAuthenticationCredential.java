c0c8a35a16c57fa17ad149514ec167bc8737d184
Ghost_chu <ghostchu@111.com>
2024-11-02 00:04:21 +0800

    自动 Trim 用户输入

--- src/main/java/com/ghostchu/btn/sparkle/module/ping/ClientAuthenticationCredential.java
+++ src/main/java/com/ghostchu/btn/sparkle/module/ping/ClientAuthenticationCredential.java
@@ -2,15 +2,29 @@ package com.ghostchu.btn.sparkle.module.ping;
 
 import java.io.Serializable;
 
-public record ClientAuthenticationCredential(
-        String appId,
-        String appSecret
-) implements Serializable {
-    public boolean isValid(){
+public class ClientAuthenticationCredential implements Serializable {
+    private String appId;
+    private String appSecret;
+
+    public ClientAuthenticationCredential(String appId, String appSecret) {
+        this.appId = appId;
+        this.appSecret = appSecret;
+    }
+
+    public String appId() {
+        return appId.trim();
+    }
+
+    public String appSecret() {
+        return appSecret.trim();
+    }
+
+    public boolean isValid() {
         return appId != null && appSecret != null;
     }
-    public void verifyOrThrow(){
-        if(!isValid()){
+
+    public void verifyOrThrow() {
+        if (!isValid()) {
             throw new IllegalArgumentException("请求未鉴权，客户端实现必须进行登录鉴权：https://github.com/PBH-BTN/BTN-Spec");
         }
     }
