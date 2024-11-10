31e04e403a312591c115654cb5e3ca9ec49e1a13
Ghost_chu <ghostchu@111.com>
2024-11-10 23:05:29 +0800

    清理审计日志

--- src/main/java/com/ghostchu/btn/sparkle/module/audit/impl/AuditRepository.java
+++ src/main/java/com/ghostchu/btn/sparkle/module/audit/impl/AuditRepository.java
@@ -3,7 +3,10 @@ package com.ghostchu.btn.sparkle.module.audit.impl;
 import com.ghostchu.btn.sparkle.module.repository.SparkleCommonRepository;
 import org.springframework.stereotype.Repository;
 
+import java.time.OffsetDateTime;
+
 @Repository
 public interface AuditRepository extends SparkleCommonRepository<Audit, Long> {
 
+    long deleteByTimestampBefore(OffsetDateTime offsetDateTime);
 }
--- src/main/java/com/ghostchu/btn/sparkle/module/cleanup/CleanupService.java
+++ src/main/java/com/ghostchu/btn/sparkle/module/cleanup/CleanupService.java
@@ -1,5 +1,6 @@
 package com.ghostchu.btn.sparkle.module.cleanup;
 
+import com.ghostchu.btn.sparkle.module.audit.impl.AuditRepository;
 import com.ghostchu.btn.sparkle.module.peerhistory.internal.PeerHistoryRepository;
 import com.ghostchu.btn.sparkle.module.snapshot.internal.SnapshotRepository;
 import lombok.extern.slf4j.Slf4j;
@@ -13,10 +14,12 @@ import java.time.OffsetDateTime;
 public class CleanupService {
     private final PeerHistoryRepository peerHistoryRepository;
     private final SnapshotRepository snapshotRepository;
+    private final AuditRepository auditRepository;
 
-    public CleanupService(PeerHistoryRepository peerHistoryRepository, SnapshotRepository snapshotRepository) {
+    public CleanupService(PeerHistoryRepository peerHistoryRepository, SnapshotRepository snapshotRepository, AuditRepository auditRepository) {
         this.peerHistoryRepository = peerHistoryRepository;
         this.snapshotRepository = snapshotRepository;
+        this.auditRepository = auditRepository;
     }
 
     // 每天凌晨 3 点清理
@@ -28,5 +31,7 @@ public class CleanupService {
         // 7 天前
         var deletedSnapshots = snapshotRepository.deleteByInsertTimeBefore(OffsetDateTime.now().minusDays(7));
         log.info("[清理] 删除了 7 天前的 Snapshot 共 {} 条", deletedSnapshots);
+        var deletedAudits = auditRepository.deleteByTimestampBefore(OffsetDateTime.now().minusDays(180));
+        log.info("[清理] 删除了 180 天前的 Audit 共 {} 条", deletedAudits);
     }
 }
