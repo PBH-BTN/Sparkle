62c6738f1f8acb256abea7412584d038d65693cf
Ghost_chu <ghostchu@111.com>
2024-12-03 02:43:34 +0800

    软删除

--- src/main/java/com/ghostchu/btn/sparkle/module/userapp/internal/UserApplicationRepository.java
+++ src/main/java/com/ghostchu/btn/sparkle/module/userapp/internal/UserApplicationRepository.java
@@ -19,9 +19,11 @@ public interface UserApplicationRepository extends SparkleCommonRepository<UserA
     @Query("select ua from UserApplication ua where ua.appId = ?1 and ua.appSecret = ?2 and ua.deletedAt is null")
     Optional<UserApplication> findByAppIdAndAppSecret(String appId, String appSecret);
 
-    @Query("select ua from UserApplication ua where ua.user.id = ?1 and ua.deletedAt is null")
+    @Query("select ua from UserApplication ua where ua.user = ?1 and ua.deletedAt is null")
     List<UserApplication> findByUser(User user);
 
-    @Query("select count(ua) from UserApplication ua where ua.user.id = ?1 and ua.deletedAt is null")
+    @Query("select count(ua) from UserApplication ua where ua.user = ?1 and ua.deletedAt is null")
     long countByUser(User user);
+
+
 }
