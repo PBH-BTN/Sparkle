a97cc78becae3d054eb018c4fb3df6dcb89b35d6
Ghost_chu <ghostchu@111.com>
2024-12-04 16:10:58 +0800

    试试注入 ObjectMapper

--- src/main/java/com/ghostchu/btn/sparkle/config/SpringRedisConfig.java
+++ src/main/java/com/ghostchu/btn/sparkle/config/SpringRedisConfig.java
@@ -1,5 +1,6 @@
 package com.ghostchu.btn.sparkle.config;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.ghostchu.btn.sparkle.module.tracker.internal.TrackedPeer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -11,22 +12,22 @@ import org.springframework.data.redis.serializer.StringRedisSerializer;
 @Configuration
 public class SpringRedisConfig {
     @Bean
-    public RedisTemplate<String, Object> redisTemplate(LettuceConnectionFactory redisConnectionFactory) {
+    public RedisTemplate<String, Object> redisTemplate(LettuceConnectionFactory redisConnectionFactory, ObjectMapper mapper) {
         RedisTemplate<String, Object> redisTemplate = new RedisTemplate<>();
         redisTemplate.setConnectionFactory(redisConnectionFactory);
         redisTemplate.setKeySerializer(new StringRedisSerializer());
-        redisTemplate.setValueSerializer(new GenericJackson2JsonRedisSerializer());
+        redisTemplate.setValueSerializer(new GenericJackson2JsonRedisSerializer(mapper));
         redisTemplate.setHashKeySerializer(new StringRedisSerializer());
         redisTemplate.setHashValueSerializer(new GenericJackson2JsonRedisSerializer());
         return redisTemplate;
     }
 
     @Bean("redisTemplateTrackedPeer")
-    public RedisTemplate<String, TrackedPeer> redisTemplateTrackedPeer(LettuceConnectionFactory redisConnectionFactory) {
+    public RedisTemplate<String, TrackedPeer> redisTemplateTrackedPeer(LettuceConnectionFactory redisConnectionFactory, ObjectMapper mapper) {
         RedisTemplate<String, TrackedPeer> redisTemplate = new RedisTemplate<>();
         redisTemplate.setConnectionFactory(redisConnectionFactory);
         redisTemplate.setKeySerializer(new StringRedisSerializer());
-        redisTemplate.setValueSerializer(new GenericJackson2JsonRedisSerializer());
+        redisTemplate.setValueSerializer(new GenericJackson2JsonRedisSerializer(mapper));
         redisTemplate.setHashKeySerializer(new StringRedisSerializer());
         redisTemplate.setHashValueSerializer(new GenericJackson2JsonRedisSerializer());
         return redisTemplate;
