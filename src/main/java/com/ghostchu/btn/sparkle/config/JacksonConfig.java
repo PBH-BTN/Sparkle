71b41aed2020feec5a2159736b6f40a87c2d0a24
Ghost_chu <ghostchu@111.com>
2024-12-04 15:58:08 +0800

    注册模块

--- src/main/java/com/ghostchu/btn/sparkle/config/JacksonConfig.java
+++ src/main/java/com/ghostchu/btn/sparkle/config/JacksonConfig.java
@@ -3,6 +3,9 @@ package com.ghostchu.btn.sparkle.config;
 import com.fasterxml.jackson.databind.DeserializationFeature;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.module.SimpleModule;
+import com.fasterxml.jackson.datatype.jdk8.Jdk8Module;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
+import com.fasterxml.jackson.module.paramnames.ParameterNamesModule;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -16,6 +19,9 @@ public class JacksonConfig {
         SimpleModule module = new SimpleModule();
         module.addDeserializer(InetAddress.class, new InetAddressDeserializer());
         mapper.registerModule(module);
+        mapper.registerModule(new ParameterNamesModule());
+        mapper.registerModule(new Jdk8Module());
+        mapper.registerModule(new JavaTimeModule());
         mapper.configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false);
         return mapper;
     }
