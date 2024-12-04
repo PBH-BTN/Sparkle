f46743116538c5f0f8a8de73dc5e8a8f34f286bc
Ghost_chu <ghostchu@111.com>
2024-12-04 16:18:16 +0800

    修复序列化错误

--- src/main/java/com/ghostchu/btn/sparkle/config/SpringRedisConfig.java
+++ src/main/java/com/ghostchu/btn/sparkle/config/SpringRedisConfig.java
@@ -7,6 +7,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.serializer.GenericJackson2JsonRedisSerializer;
+import org.springframework.data.redis.serializer.Jackson2JsonRedisSerializer;
 import org.springframework.data.redis.serializer.StringRedisSerializer;
 
 @Configuration
@@ -27,9 +28,9 @@ public class SpringRedisConfig {
         RedisTemplate<String, TrackedPeer> redisTemplate = new RedisTemplate<>();
         redisTemplate.setConnectionFactory(redisConnectionFactory);
         redisTemplate.setKeySerializer(new StringRedisSerializer());
-        redisTemplate.setValueSerializer(new GenericJackson2JsonRedisSerializer(mapper));
+        redisTemplate.setValueSerializer(new Jackson2JsonRedisSerializer<>(mapper, TrackedPeer.class));
         redisTemplate.setHashKeySerializer(new StringRedisSerializer());
-        redisTemplate.setHashValueSerializer(new GenericJackson2JsonRedisSerializer(mapper));
+        redisTemplate.setHashValueSerializer(new Jackson2JsonRedisSerializer<>(mapper, TrackedPeer.class));
         return redisTemplate;
     }
 
