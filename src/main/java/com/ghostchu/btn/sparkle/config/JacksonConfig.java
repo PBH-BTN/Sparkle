0b0b84ac79af4fd062a9df4b638792236527dfc8
Ghost_chu <ghostchu@111.com>
2024-12-04 16:07:11 +0800

    标记 primary

--- src/main/java/com/ghostchu/btn/sparkle/config/JacksonConfig.java
+++ src/main/java/com/ghostchu/btn/sparkle/config/JacksonConfig.java
@@ -8,12 +8,14 @@ import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 import com.fasterxml.jackson.module.paramnames.ParameterNamesModule;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
 
 import java.net.InetAddress;
 
 @Configuration
 public class JacksonConfig {
     @Bean
+    @Primary
     public ObjectMapper objectMapper() {
         ObjectMapper mapper = new ObjectMapper();
         SimpleModule module = new SimpleModule();
