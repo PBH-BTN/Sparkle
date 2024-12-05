b0aecd2b05d6159ff171de3c3f754d1a93499640
Ghost_chu <ghostchu@111.com>
2024-12-05 21:23:40 +0800

    3 并发查询

--- src/main/java/com/ghostchu/btn/sparkle/util/DatabaseCare.java
+++ src/main/java/com/ghostchu/btn/sparkle/util/DatabaseCare.java
@@ -3,5 +3,5 @@ package com.ghostchu.btn.sparkle.util;
 import java.util.concurrent.Semaphore;
 
 public class DatabaseCare {
-    public static final Semaphore generateParallel = new Semaphore(1);
+    public static final Semaphore generateParallel = new Semaphore(3);
 }
