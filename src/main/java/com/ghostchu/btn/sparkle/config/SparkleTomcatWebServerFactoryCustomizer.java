59a760c3326d5c49e29ebc4e4ee6b405a78af11c
Ghost_chu <ghostchu@111.com>
2024-12-14 21:54:35 +0800

    加不上，不知道为什么不好用，就先这样

--- src/main/java/com/ghostchu/btn/sparkle/config/SparkleTomcatWebServerFactoryCustomizer.java
+++ src/main/java/com/ghostchu/btn/sparkle/config/SparkleTomcatWebServerFactoryCustomizer.java
@@ -1,27 +1,27 @@
-package com.ghostchu.btn.sparkle.config;
-
-import org.apache.commons.lang3.StringUtils;
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
-import org.springframework.boot.web.server.WebServerFactoryCustomizer;
-import org.springframework.stereotype.Component;
-
-import java.io.File;
-
-@Component
-public class SparkleTomcatWebServerFactoryCustomizer implements WebServerFactoryCustomizer<TomcatServletWebServerFactory> {
-    @Value("${server.unixsocketpath}")
-    private String unixSocketPath;
-
-    @Override
-    public void customize(TomcatServletWebServerFactory factory) {
-        if (StringUtils.isEmpty(unixSocketPath)) {
-            return;
-        }
-        File file = new File(unixSocketPath);
-        if (file.exists()) {
-            file.delete();
-        }
-        factory.addConnectorCustomizers(connector -> connector.setProperty("unixDomainSocketPath", unixSocketPath));
-    }
-}
+//package com.ghostchu.btn.sparkle.config;
+//
+//import org.apache.commons.lang3.StringUtils;
+//import org.springframework.beans.factory.annotation.Value;
+//import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
+//import org.springframework.boot.web.server.WebServerFactoryCustomizer;
+//import org.springframework.stereotype.Component;
+//
+//import java.io.File;
+//
+//@Component
+//public class SparkleTomcatWebServerFactoryCustomizer implements WebServerFactoryCustomizer<TomcatServletWebServerFactory> {
+//    @Value("${server.unixsocketpath}")
+//    private String unixSocketPath;
+//
+//    @Override
+//    public void customize(TomcatServletWebServerFactory factory) {
+//        if (StringUtils.isEmpty(unixSocketPath)) {
+//            return;
+//        }
+//        File file = new File(unixSocketPath);
+//        if (file.exists()) {
+//            file.delete();
+//        }
+//        factory.addConnectorCustomizers(connector -> connector.setProperty("unixDomainSocketPath", unixSocketPath));
+//    }
+//}
