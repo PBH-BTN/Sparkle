d1239cf93780843128f6d5d8eeb83bc41ece6103
Ghost_chu <ghostchu@111.com>
2024-12-14 21:09:18 +0800

    支持 unix socket path

--- src/main/java/com/ghostchu/btn/sparkle/config/SparkleTomcatWebServerFactoryCustomizer.java
+++ src/main/java/com/ghostchu/btn/sparkle/config/SparkleTomcatWebServerFactoryCustomizer.java
@@ -4,10 +4,12 @@ import org.apache.commons.lang3.StringUtils;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
 import org.springframework.boot.web.server.WebServerFactoryCustomizer;
-import org.springframework.context.annotation.Configuration;
+import org.springframework.stereotype.Component;
 
-@Configuration
-public class TomcatWebServerFactoryCustomizer implements WebServerFactoryCustomizer<TomcatServletWebServerFactory> {
+import java.io.File;
+
+@Component
+public class SparkleTomcatWebServerFactoryCustomizer implements WebServerFactoryCustomizer<TomcatServletWebServerFactory> {
     @Value("${server.unixsocketpath}")
     private String unixSocketPath;
 
@@ -15,6 +17,10 @@ public class TomcatWebServerFactoryCustomizer implements WebServerFactoryCustomi
     public void customize(TomcatServletWebServerFactory factory) {
         if (StringUtils.isEmpty(unixSocketPath)) {
             return;
+        }
+        File file = new File(unixSocketPath);
+        if (file.exists()) {
+            file.delete();
         }
         factory.addConnectorCustomizers(connector -> connector.setProperty("unixDomainSocketPath", unixSocketPath));
     }
