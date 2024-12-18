99307b3f9db199670bd0da7bc3a90c26316a627f
Ghost_chu <ghostchu@111.com>
2024-12-19 05:28:07 +0800

    索引

--- src/main/java/com/ghostchu/btn/sparkle/SparkleApplication.java
+++ src/main/java/com/ghostchu/btn/sparkle/SparkleApplication.java
@@ -4,6 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.stereotype.Indexed;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 @SpringBootApplication
@@ -11,6 +12,7 @@ import org.springframework.transaction.annotation.EnableTransactionManagement;
 @EnableScheduling
 @EnableTransactionManagement
 @EnableJpaRepositories
+@Indexed
 public class SparkleApplication {
 
     public static void main(String[] args) {
