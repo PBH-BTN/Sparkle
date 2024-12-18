c8f9ba90b65c62cf036d22df33ffb67e2565d88e
Ghost_chu <ghostchu@111.com>
2024-12-19 04:48:15 +0800

    还是可以有

--- src/main/java/com/ghostchu/btn/sparkle/SparkleApplication.java
+++ src/main/java/com/ghostchu/btn/sparkle/SparkleApplication.java
@@ -3,11 +3,12 @@ package com.ghostchu.btn.sparkle;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 @SpringBootApplication
-//@EnableAsync
+@EnableAsync
 @EnableScheduling
 @EnableTransactionManagement
 @EnableJpaRepositories
