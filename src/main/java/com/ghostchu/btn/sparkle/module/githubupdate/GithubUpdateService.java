f24efa586fc239bb0ecd4881e6ea82150bb832eb
Ghost_chu <ghostchu@111.com>
2024-12-28 23:16:15 +0800

    只留四个规则

--- src/main/java/com/ghostchu/btn/sparkle/module/githubupdate/GithubUpdateService.java
+++ src/main/java/com/ghostchu/btn/sparkle/module/githubupdate/GithubUpdateService.java
@@ -3,17 +3,14 @@ package com.ghostchu.btn.sparkle.module.githubupdate;
 import com.ghostchu.btn.sparkle.module.analyse.AnalyseService;
 import com.ghostchu.btn.sparkle.module.analyse.impl.AnalysedRule;
 import com.ghostchu.btn.sparkle.module.banhistory.BanHistoryService;
-import com.ghostchu.btn.sparkle.module.banhistory.internal.BanHistory;
 import com.ghostchu.btn.sparkle.module.banhistory.internal.BanHistoryRepository;
 import com.ghostchu.btn.sparkle.util.IPUtil;
 import jakarta.transaction.Transactional;
-import lombok.Cleanup;
 import lombok.extern.slf4j.Slf4j;
 import org.kohsuke.github.GHRepository;
 import org.kohsuke.github.GitHub;
 import org.kohsuke.github.GitHubBuilder;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.data.jpa.domain.Specification;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
 
@@ -21,7 +18,6 @@ import java.io.IOException;
 import java.nio.charset.StandardCharsets;
 import java.time.OffsetDateTime;
 import java.time.temporal.ChronoUnit;
-import java.util.Arrays;
 import java.util.StringJoiner;
 import java.util.function.Supplier;
 
@@ -62,13 +58,6 @@ public class GithubUpdateService {
         updateFile(repository, "untrusted-ips.txt", () -> generateUntrustedIps().getBytes(StandardCharsets.UTF_8));
         updateFile(repository, "high-risk-ips.txt", () -> generateHighRiskIps().getBytes(StandardCharsets.UTF_8));
         updateFile(repository, "overdownload-ips.txt", () -> generateOverDownloadIps().getBytes(StandardCharsets.UTF_8));
-        //updateFile(repository, "strange_ipv6_block.txt", () -> generateStrangeIPV6().getBytes(StandardCharsets.UTF_8));
-        updateFile(repository, "random-peerid.txt", () -> generateGopeedDev().getBytes(StandardCharsets.UTF_8));
-        updateFile(repository, "hp_torrent.txt", () -> generateHpTorrents().getBytes(StandardCharsets.UTF_8));
-        updateFile(repository, "dt_torrent.txt", () -> generateDtTorrents().getBytes(StandardCharsets.UTF_8));
-        //updateFile(repository, "go.torrent dev 20181121.txt", () -> generateBaiduNetdisk().getBytes(StandardCharsets.UTF_8));
-        updateFile(repository, "0xde-0xad-0xbe-0xef.txt", () -> generateDeadBeef().getBytes(StandardCharsets.UTF_8));
-        updateFile(repository, "123pan.txt", () -> generate123pan().getBytes(StandardCharsets.UTF_8));
         updateFile(repository, "tracker-high-risk-ips.txt", () -> generateTrackerHighRiskIps().getBytes(StandardCharsets.UTF_8));
     }
 
@@ -110,38 +99,38 @@ public class GithubUpdateService {
 //        return strJoiner.toString();
 //    }
 
-    private String generateGopeedDev() {
-        var strJoiner = new StringJoiner("\n");
-        banHistoryRepository.findAllByPaging((Specification<BanHistory>) (root, query, criteriaBuilder) -> {
-            if (query != null)
-                query.distinct(true);
-            return criteriaBuilder.or(
-                    criteriaBuilder.like(root.get("peerClientName"), "Gopeed dev%"),
-                    criteriaBuilder.like(root.get("peerId"), "-gp%")
-            );
-        }, (page) -> page.forEach(ban -> strJoiner.add(ban.getPeerIp().getHostAddress())));
-        return strJoiner.toString();
-    }
-
-    private String generate123pan() {
-        var strJoiner = new StringJoiner("\n");
-        banHistoryRepository.findAllByPaging((Specification<BanHistory>) (root, query, criteriaBuilder) -> {
-            if (query != null)
-                query.distinct(true);
-            return criteriaBuilder.like(root.get("peerClientName"), "offline-download (devel) (anacrolix/torrent unknown)%");
-        }, (page) -> page.forEach(ban -> strJoiner.add(ban.getPeerIp().getHostAddress())));
-        return strJoiner.toString();
-    }
-
-    private String generateDeadBeef() {
-        var strJoiner = new StringJoiner("\n");
-        banHistoryRepository.findAllByPaging((Specification<BanHistory>) (root, query, criteriaBuilder) -> {
-            if (query != null)
-                query.distinct(true);
-            return criteriaBuilder.like(root.get("peerClientName"), "ޭ__%");
-        }, (page) -> page.forEach(ban -> strJoiner.add(ban.getPeerIp().getHostAddress())));
-        return strJoiner.toString();
-    }
+//    private String generateGopeedDev() {
+//        var strJoiner = new StringJoiner("\n");
+//        banHistoryRepository.findAllByPaging((Specification<BanHistory>) (root, query, criteriaBuilder) -> {
+//            if (query != null)
+//                query.distinct(true);
+//            return criteriaBuilder.or(
+//                    criteriaBuilder.like(root.get("peerClientName"), "Gopeed dev%"),
+//                    criteriaBuilder.like(root.get("peerId"), "-gp%")
+//            );
+//        }, (page) -> page.forEach(ban -> strJoiner.add(ban.getPeerIp().getHostAddress())));
+//        return strJoiner.toString();
+//    }
+//
+//    private String generate123pan() {
+//        var strJoiner = new StringJoiner("\n");
+//        banHistoryRepository.findAllByPaging((Specification<BanHistory>) (root, query, criteriaBuilder) -> {
+//            if (query != null)
+//                query.distinct(true);
+//            return criteriaBuilder.like(root.get("peerClientName"), "offline-download (devel) (anacrolix/torrent unknown)%");
+//        }, (page) -> page.forEach(ban -> strJoiner.add(ban.getPeerIp().getHostAddress())));
+//        return strJoiner.toString();
+//    }
+//
+//    private String generateDeadBeef() {
+//        var strJoiner = new StringJoiner("\n");
+//        banHistoryRepository.findAllByPaging((Specification<BanHistory>) (root, query, criteriaBuilder) -> {
+//            if (query != null)
+//                query.distinct(true);
+//            return criteriaBuilder.like(root.get("peerClientName"), "ޭ__%");
+//        }, (page) -> page.forEach(ban -> strJoiner.add(ban.getPeerIp().getHostAddress())));
+//        return strJoiner.toString();
+//    }
 
     private String generateBaiduNetdisk() {
         var strJoiner = new StringJoiner("\n");
@@ -158,47 +147,39 @@ public class GithubUpdateService {
         return strJoiner.toString();
     }
 
-
-    private String generateDtTorrents() {
-        var strJoiner = new StringJoiner("\n");
-        banHistoryRepository.findAllByPaging((Specification<BanHistory>) (root, query, criteriaBuilder) -> {
-            if (query != null)
-                query.distinct(true);
-            return criteriaBuilder.or(
-                    criteriaBuilder.like(root.get("peerClientName"), "dt/torrent%"),
-                    criteriaBuilder.like(root.get("peerId"), "-DT%")
-            );
-        }, (page) -> page.forEach(ban -> strJoiner.add(ban.getPeerIp().getHostAddress())));
-        return strJoiner.toString();
-    }
-
-    private String generateHpTorrents() {
-        var strJoiner = new StringJoiner("\n");
-        banHistoryRepository.findAllByPaging((Specification<BanHistory>) (root, query, criteriaBuilder) -> {
-            if (query != null)
-                query.distinct(true);
-            return criteriaBuilder.or(
-                    criteriaBuilder.like(root.get("peerClientName"), "hp/torrent%"),
-                    criteriaBuilder.like(root.get("peerId"), "-HP%")
-            );
-        }, (page) -> page.forEach(ban -> strJoiner.add(ban.getPeerIp().getHostAddress())));
-        return strJoiner.toString();
-    }
-
+    //
+//
+//    private String generateDtTorrents() {
+//        var strJoiner = new StringJoiner("\n");
+//        banHistoryRepository.findAllByPaging((Specification<BanHistory>) (root, query, criteriaBuilder) -> {
+//            if (query != null)
+//                query.distinct(true);
+//            return criteriaBuilder.or(
+//                    criteriaBuilder.like(root.get("peerClientName"), "dt/torrent%"),
+//                    criteriaBuilder.like(root.get("peerId"), "-DT%")
+//            );
+//        }, (page) -> page.forEach(ban -> strJoiner.add(ban.getPeerIp().getHostAddress())));
+//        return strJoiner.toString();
+//    }
+//
+//    private String generateHpTorrents() {
+//        var strJoiner = new StringJoiner("\n");
+//        banHistoryRepository.findAllByPaging((Specification<BanHistory>) (root, query, criteriaBuilder) -> {
+//            if (query != null)
+//                query.distinct(true);
+//            return criteriaBuilder.or(
+//                    criteriaBuilder.like(root.get("peerClientName"), "hp/torrent%"),
+//                    criteriaBuilder.like(root.get("peerId"), "-HP%")
+//            );
+//        }, (page) -> page.forEach(ban -> strJoiner.add(ban.getPeerIp().getHostAddress())));
+//        return strJoiner.toString();
+//    }
+//
     private void updateFile(GHRepository repository, String file, Supplier<byte[]> contentSupplier) {
         try {
             var oldFile = repository.getFileContent(file);
             var sha = oldFile != null ? oldFile.getSha() : null;
             var content = contentSupplier.get();
-            if (oldFile != null) {
-                @Cleanup
-                var is = oldFile.read();
-                var oldData = is.readAllBytes();
-                if (Arrays.equals(content, oldData)) {
-                    log.info("{}: 无需更新，跳过", file);
-                    return;
-                }
-            }
             var commitResponse = repository.createContent()
                     .path(file)
                     .branch(branchName)
@@ -207,7 +188,7 @@ public class GithubUpdateService {
                     .content(content)
                     .commit();
             var commit = commitResponse.getCommit();
-            log.info("GitHub 同步规则 “{}” 更新结果：Sha: {} 新的文件行数: {}", file, commit.getSHA1(), file.lines().count());
+            log.info("GitHub 同步规则 “{}” 更新结果：Sha: {}", file, commit.getSHA1());
         } catch (Throwable e) {
             log.error("无法完成数据更新操作", e);
         }
