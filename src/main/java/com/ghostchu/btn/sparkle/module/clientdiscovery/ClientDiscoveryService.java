32b1ea01cfd95dddf0f490f3eca729e1959354d7
Ghost_chu <ghostchu@111.com>
2025-01-07 03:41:32 +0800

    update

--- src/main/java/com/ghostchu/btn/sparkle/module/clientdiscovery/ClientDiscoveryService.java
+++ src/main/java/com/ghostchu/btn/sparkle/module/clientdiscovery/ClientDiscoveryService.java
@@ -5,10 +5,12 @@ import com.ghostchu.btn.sparkle.module.clientdiscovery.internal.ClientDiscoveryR
 import com.ghostchu.btn.sparkle.util.ByteUtil;
 import com.ghostchu.btn.sparkle.util.paging.SparklePage;
 import io.micrometer.core.instrument.MeterRegistry;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.cache.annotation.Cacheable;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.domain.Specification;
+import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Isolation;
 import org.springframework.transaction.annotation.Propagation;
@@ -19,6 +21,7 @@ import java.time.OffsetDateTime;
 import java.util.Collection;
 
 @Service
+@Slf4j
 public class ClientDiscoveryService {
     private final ClientDiscoveryRepository clientDiscoveryRepository;
     @Autowired
@@ -29,10 +32,15 @@ public class ClientDiscoveryService {
     }
 
     @Transactional(propagation = Propagation.REQUIRES_NEW, isolation = Isolation.READ_UNCOMMITTED)
+    @Async
     public void handleIdentities(OffsetDateTime timeForFoundAt, OffsetDateTime timeForLastSeenAt, Collection<ClientIdentity> clientIdentities) {
         meterRegistry.counter("sparkle_client_discovery_processed").increment();
         for (ClientIdentity ci : clientIdentities) {
-            clientDiscoveryRepository.saveIgnoreConflict(ci.hash(), ByteUtil.filterUTF8(ci.getClientName()), ByteUtil.filterUTF8(ci.getPeerId()), timeForFoundAt);
+            try {
+                clientDiscoveryRepository.saveIgnoreConflict(ci.hash(), ByteUtil.filterUTF8(ci.getClientName()), ByteUtil.filterUTF8(ci.getPeerId()), timeForFoundAt);
+            } catch (Exception e) {
+                log.error("Failed to save client discovery: {}:{}", e.getClass().getName(), e.getMessage());
+            }
         }
     }
 
