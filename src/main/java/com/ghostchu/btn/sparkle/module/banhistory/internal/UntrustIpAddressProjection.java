8d5fdc2454943139f86277c155b2689ed65e67e1
Ghost_chu <ghostchu@111.com>
2025-01-07 01:04:58 +0800

    fix projection

--- src/main/java/com/ghostchu/btn/sparkle/module/banhistory/internal/UntrustIpAddressProjection.java
+++ src/main/java/com/ghostchu/btn/sparkle/module/banhistory/internal/UntrustIpAddressProjection.java
@@ -1,13 +1,9 @@
 package com.ghostchu.btn.sparkle.module.banhistory.internal;
 
-import lombok.AllArgsConstructor;
-import lombok.Data;
-
 import java.net.InetAddress;
 
-@AllArgsConstructor
-@Data
-public class UntrustIpAddressProjection {
-    private InetAddress peerIp;
-    private Integer count;
+public interface UntrustIpAddressProjection {
+    InetAddress getPeerIp();
+
+    Integer getCount();
 }
